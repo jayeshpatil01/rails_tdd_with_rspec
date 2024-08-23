f7b4f694c6f688b6106ecb48d2d201f805b6ece7
Jay <jayeshpatil8041@gmail.com>
2024-08-23 20:44:25 +0530

    capybara testing with rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'status', to: 'status#index'
   get 'consoles', to: 'consoles#index'
+  get 'home', to: 'home#index'
+  get 'about', to: 'about#index'
 end
---
+++ spec/features/home_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Home features', type: :feature do
+  it 'displays the name of the app and links to the About page' do
+    visit('/home')
+    expect(page).to have_content('Game Tracker')
+    click_link('About')
+    expect(current_path).to eq('/about')
+    expect(page).to have_content('About')
+  end
+end
